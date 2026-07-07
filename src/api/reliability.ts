68a15bc7f71d307c2fe58f541329172fd6581e61
Eugeniu Marinescu <marinescuevghenii@gmail.com>
2026-07-07 03:20:12 +0200

    docs: updates screenshots

--- src/api/reliability.ts
+++ src/api/reliability.ts
@@ -2,5 +2,5 @@ import api from './api.ts';
 import type { TypeReliability } from '../types/reliability.ts';
 
 export async function fetchReliability({ userId, from }: { userId: string; from: string }): Promise<TypeReliability> {
-	return await api.get(`/api/users/user_${userId}/reliability?from=${from}`);
+	return await api.get(`/api/users/user_${userId}/reliabilitys?from=${from}`);
 }
